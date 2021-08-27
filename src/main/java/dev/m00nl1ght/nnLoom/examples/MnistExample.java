0ec02a7235d2023cdf8394be7681eda5f8dd8a1a
m00nl1ght <github@m00nl1ght.dev>
2021-08-27 23:40:39 +0200

    optimize mnist example

--- src/main/java/dev/m00nl1ght/nnLoom/examples/MnistExample.java
+++ src/main/java/dev/m00nl1ght/nnLoom/examples/MnistExample.java
@@ -4,6 +4,8 @@ import dev.m00nl1ght.nnLoom.*;
 import dev.m00nl1ght.nnLoom.mnist.MnistReader;
 
 import java.nio.FloatBuffer;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 
 public class MnistExample {
@@ -23,7 +25,8 @@ public class MnistExample {
     public static void run(NNPlatform nnPlatform, long initSeed) {
 
         final var network = NNetwork.builder(MNIST_PX * MNIST_PX)
-                .layerFC(100, Activation.Tanh, Initialisation.Xavier)
+                .layerFC(42, Activation.Tanh, Initialisation.Xavier)
+                .layerFC(10, Activation.Tanh, Initialisation.Xavier)
                 .layerFC(MNIST_CAT, Activation.Sigmoid, Initialisation.Uniform)
                 .build();
 
@@ -35,27 +38,50 @@ public class MnistExample {
         final var dataTe = MnistReader.readData(cl.getResourceAsStream(RES_DATA_TE));
         final var labelsTe = MnistReader.readLabels(cl.getResourceAsStream(RES_LABELS_TE));
 
-        eval(nnPlatform, network, dataTe, labelsTe);
+        eval(nnPlatform, network, dataTe, labelsTe, MNIST_SIZE_TE, 0);
 
-        nnPlatform.train(network, MNIST_SIZE_TR / 12, dataTr, labelsTr, 1, 0.01f);
-        eval(nnPlatform, network, dataTe, labelsTe);
+        nnPlatform.train(network, MNIST_SIZE_TR, dataTr, labelsTr, 3, 0.01f);
 
-        nnPlatform.train(network, MNIST_SIZE_TR / 6, dataTr, labelsTr, 1, 0.01f);
-        eval(nnPlatform, network, dataTe, labelsTe);
-
-        nnPlatform.train(network, MNIST_SIZE_TR / 3, dataTr, labelsTr, 1, 0.01f);
-        eval(nnPlatform, network, dataTe, labelsTe);
-
-        nnPlatform.train(network, MNIST_SIZE_TR, dataTr, labelsTr, 1, 0.01f);
-        eval(nnPlatform, network, dataTe, labelsTe);
+        eval(nnPlatform, network, dataTe, labelsTe, MNIST_SIZE_TE, 5);
 
     }
 
-    private static void eval(NNPlatform nnPlatform, NNetwork network, FloatBuffer data, FloatBuffer labels) {
+    private static void eval(NNPlatform nnPlatform, NNetwork network, FloatBuffer data, FloatBuffer labels, int count, int incEx) {
         data.clear(); labels.clear();
-        final var results = nnPlatform.predict(network, MNIST_SIZE_TE, data);
-        final var error = nnPlatform.eval(results, labels, ErrorFunction.ME, MNIST_CAT, MNIST_SIZE_TE);
-        System.out.printf("Error is now: %.3f%n", error);
+
+        final var results = nnPlatform.predict(network, count, data);
+        final var error = nnPlatform.eval(results, labels, ErrorFunction.MSE, MNIST_CAT, count);
+        results.clear(); labels.clear();
+
+        final var incDist = new int[MNIST_CAT];
+        final var incorrect = new ArrayList<Integer>();
+        for (int i = 0; i < count; i++) {
+            final var pred = maxResult(results, i);
+            final var label = maxResult(labels, i);
+            if (pred != label) {
+                incorrect.add(i);
+                incDist[label]++;
+            }
+        }
+
+        final float incP = 100f * incorrect.size() / count;
+        System.out.printf("Incorrect predictions in total: " + incorrect.size() + " (%.2f %%)%n", incP);
+        if (incEx > 0) for (int i = 0; i < MNIST_CAT; i++) {
+            System.out.println("Incorrect predictions in category " + i + ": " + incDist[i]);
+        }
+
+        System.out.printf("MSE is now: %.3f%n", error);
+
+        Collections.shuffle(incorrect);
+        if (incEx > 0 && !incorrect.isEmpty()) {
+            System.out.println("Examples for incorrect predictions:");
+            for (int i = 0; i < incEx && i < incorrect.size(); i++) {
+                final var idx = incorrect.get(i);
+                printImage(data, labels, idx);
+                printResults(results, labels, idx);
+            }
+        }
+
         data.clear(); labels.clear();
     }
 
@@ -64,13 +90,15 @@ public class MnistExample {
         data.clear();
         data.position(idx * MNIST_PX * MNIST_PX);
 
-        System.out.println("Image " + idx + " with label " + label(labels, idx) + ":");
+        System.out.println("Image " + idx + " with label " + maxResult(labels, idx) + ":");
         System.out.println("#".repeat(MNIST_PX + 2));
         for (int i = 0; i < MNIST_PX; i++) {
             System.out.print("#");
             for (int j = 0; j < MNIST_PX; j++) {
                 final var v = data.get();
-                System.out.print(v > 0.5f ? "O" : " ");
+                if (v > 0.7f) System.out.print("O");
+                else if (v > 0.4f) System.out.print("-");
+                else System.out.print(" ");
             }
             System.out.print("#");
             System.out.println();
@@ -83,22 +111,27 @@ public class MnistExample {
 
     private static void printResults(FloatBuffer results, FloatBuffer labels, int idx) {
 
-        System.out.println("Results for image " + idx + " with label " + label(labels, idx) + ":");
+        System.out.println("Results for image " + idx + " with label " + maxResult(labels, idx) + ":");
 
         results.clear();
 
         for (int i = 0; i < MNIST_CAT; i++) {
-            System.out.printf(i + " -> %.3f%n", results.get());
+            System.out.printf(i + " -> %.3f%n", results.get(idx * MNIST_CAT + i));
         }
 
         results.clear();
     }
 
-    private static int label(FloatBuffer labels, int idx) {
-        final var labelA = new float[MNIST_CAT];
-        labels.get(idx * MNIST_CAT, labelA);
-        int label = 0; for (int i = 0; i < MNIST_CAT && labelA[i] < 1f; i++) label = i + 1;
-        return label;
+    private static int maxResult(FloatBuffer data, int idx) {
+        final var vals = new float[MNIST_CAT];
+        data.get(idx * MNIST_CAT, vals);
+
+        int maxAt = 0;
+        for (int i = 1; i < vals.length; i++) {
+            maxAt = vals[i] > vals[maxAt] ? i : maxAt;
+        }
+
+        return maxAt;
     }
 
 }
