96eb5a9fbe8082f0b22f29d2ec20cec5696633a7
m00nl1ght <github@m00nl1ght.dev>
2021-08-30 14:09:19 +0200

    add cmd args

--- src/main/java/dev/m00nl1ght/nnLoom/Main.java
+++ src/main/java/dev/m00nl1ght/nnLoom/Main.java
@@ -1,6 +1,7 @@
 package dev.m00nl1ght.nnLoom;
 
 import dev.m00nl1ght.nnLoom.examples.MnistExample;
+import dev.m00nl1ght.nnLoom.examples.XorExample;
 import dev.m00nl1ght.nnLoom.opencl.CLContext;
 import dev.m00nl1ght.nnLoom.opencl.CLDevice;
 import dev.m00nl1ght.nnLoom.profiler.DebugProfiler;
@@ -16,29 +17,42 @@ public class Main {
 
     public static void main(String[] args) {
 
-        final var initSeed = new Random().nextLong();
+        final var argExample = args.length > 0 ? args[0] : "mnist";
+        final var argPlatform = args.length > 1 ? args[1] : "opencl2d";
+        final var argSeed = args.length > 2 ? args[2] : null;
+
         final var devs = CLDevice.getAvailableDevices(CL_DEVICE_TYPE_CPU);
+        final var initSeed = argSeed == null ? new Random().nextLong() : argSeed.hashCode();
 
         for (CLDevice device : devs) {
 
             // if (!device.getPlatformInfo(CL_PLATFORM_NAME).contains("NVIDIA")) continue;
 
             System.out.println("--------------------------------------------------------------");
-            System.out.println("Platform name: " + device.getPlatformInfo(CL_PLATFORM_NAME));
-            System.out.println("Device name: " + device.getDeviceInfo(CL_DEVICE_NAME));
+            System.out.println("OpenCL Platform name: " + device.getPlatformInfo(CL_PLATFORM_NAME));
+            System.out.println("OpenCL Device name: " + device.getDeviceInfo(CL_DEVICE_NAME));
             System.out.println("--------------------------------------------------------------");
 
             final var context = CLContext.create(device);
 
-            final var nnPlatform = new NNPlatformOpenCL2d(context);
+            final var nnPlatform = switch (argPlatform) {
+                case "java" -> new NNPlatformJava();
+                case "opencl" -> new NNPlatformOpenCL(context);
+                case "opencl2d" -> new NNPlatformOpenCL2d(context);
+                default -> throw new IllegalArgumentException("Invalid platform: " + argPlatform);
+            };
+
             nnPlatform.init();
 
             final var profilerGroup = nnPlatform.attachDefaultProfilers().stream().findFirst().orElseThrow();
             final var profiler = new DebugProfiler();
             profiler.addGroup(profilerGroup);
 
-            // XorExample.run(nnPlatform, initSeed);
-            MnistExample.run(nnPlatform, initSeed);
+            switch (argExample) {
+                case "xor" -> XorExample.run(nnPlatform, initSeed);
+                case "mnist" -> MnistExample.run(nnPlatform, initSeed);
+                default -> throw new IllegalArgumentException("Invalid example: " + argExample);
+            }
 
             System.out.println(DebugUtils.printProfilerInfo(profiler));
 
